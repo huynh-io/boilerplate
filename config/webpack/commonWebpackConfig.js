f9257e2f46eccdfc6ab9ae6e2e5df5cf095f3401
John Huynh <john@huynh.io>
2024-08-26 14:05:25 -0500

    Get dev server working

--- config/webpack/commonWebpackConfig.js
+++ config/webpack/commonWebpackConfig.js
@@ -1,16 +1,17 @@
-// The source code including full typescript support is available at: 
+// The source code including full typescript support is available at:
 // https://github.com/shakacode/react_on_rails_demo_ssr_hmr/blob/master/config/webpack/commonWebpackConfig.js
 
 // Common configuration applying to client and server configuration
-const { webpackConfig: baseClientWebpackConfig, merge } = require('shakapacker');
+const { generateWebpackConfig, merge } = require("shakapacker");
 
 const commonOptions = {
   resolve: {
-    extensions: ['.css', '.ts', '.tsx'],
+    extensions: [".css", ".ts", ".tsx"],
   },
 };
 
 // Copy the object using merge b/c the baseClientWebpackConfig and commonOptions are mutable globals
-const commonWebpackConfig = () => merge({}, baseClientWebpackConfig, commonOptions);
+const commonWebpackConfig = () =>
+  merge({}, generateWebpackConfig(), commonOptions);
 
 module.exports = commonWebpackConfig;
