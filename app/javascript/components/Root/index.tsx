bcd5af3f68e7826016091e8fd9b46722dfa0887f
John Huynh <john@huynh.io>
2024-08-27 00:34:24 -0500

    Add initial Root component that will render all the children screens

--- app/javascript/components/Root/index.tsx
+++ app/javascript/components/Root/index.tsx
@@ -1,17 +1,13 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
-const Root = () => <div>Root</div>;
+const Root = () => {
+  return (
+    <>
+      <div>Hi</div>
+      <Outlet />
+    </>
+  );
+};
 
 export default Root;
-
-// import { RouterProvider } from "react-router-dom";
-// import { Router } from "./Router";
-// // import { Provider } from "react-redux";
-
-// // See documentation for https://github.com/reactjs/react-redux.
-// // This is how you get props from the Rails view into the redux store.
-// // This code here binds your smart component to the redux store.
-
-// const App = (props) => <RouterProvider router={Router} />;
-
-// export default App;
