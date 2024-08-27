70bbde8a4eb4ce358c299e74f8d0593e07b47c6b
John Huynh <john@huynh.io>
2024-08-27 13:30:05 -0500

    Allow delegate most routes to react router

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 Rails.application.routes.draw do
   root 'application#index'
+  # Rails matches routes from top to bottom. Everything that doesn't hit an API route above will defer to React Router.
+  get '*path', to: 'application#index'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
