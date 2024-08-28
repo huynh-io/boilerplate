edefb90d3f7111084e2f1ca84aeec44f821a7181
John Huynh <john@huynh.io>
2024-08-28 14:24:06 -0500

    [boilerplate][feat] move users controller into expect api namespace

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def index
+        @users = User.all
+
+        # Render
+      end
+
+      def create
+        @user = UserCreator.call(user_params)
+
+        # render API response
+      end
+
+      private
+
+      def user_params
+        params.permit(:email, :refresh_token, :custom_metadata, :email_verified)
+      end
+    end
+  end
+end
