51c305eaafc20c369a79ea8a95c71d4c0aa610d5
John Huynh <john@huynh.io>
2024-08-27 23:20:41 -0500

    [boilerplate][feat] add initial root layout

--- app/javascript/components/Root/Root.tsx
+++ app/javascript/components/Root/Root.tsx
@@ -2,29 +2,107 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import { RootState } from "store";
-import { login, logout } from "store/Authentication";
+import {
+  Navbar,
+  NavbarItem,
+  NavbarSection,
+  NavbarSpacer,
+} from "components/Catalyst/navbar";
+import {
+  Sidebar,
+  SidebarItem,
+  SidebarSection,
+  SidebarSpacer,
+} from "components/Catalyst/sidebar";
+import { StackedLayout } from "components/Catalyst/stacked-layout";
+
+// const Root = () => {
+//   const authenticated = useSelector(
+//     (state: RootState) => state.authentication.authenticated
+//   );
+//   const dispatch = useDispatch();
+
+//   return (
+//     <>
+//       <div>Root</div>
+//       <div>{authenticated ? "Logged In" : "Logged Out"}</div>
+//       <button aria-label="Signup" onClick={() => console.log("1")}>
+//         Signup
+//       </button>
+//       <button aria-label="Login" onClick={() => dispatch(login())}>
+//         Login
+//       </button>
+//       <button aria-label="Login" onClick={() => dispatch(logout())}>
+//         Logout
+//       </button>
+//       <Outlet />
+//     </>
+//   );
+// };
 
 const Root = () => {
   const authenticated = useSelector(
     (state: RootState) => state.authentication.authenticated
   );
-  const dispatch = useDispatch();
+
+  const sidebar = (
+    <Sidebar>
+      <SidebarItem href="/" aria-label="Home" current>
+        Home
+      </SidebarItem>
+
+      <SidebarSpacer />
+
+      {authenticated ? (
+        <SidebarSection>
+          <SidebarItem href="/logOut" aria-label="Log Out">
+            Log Out
+          </SidebarItem>
+        </SidebarSection>
+      ) : (
+        <SidebarSection>
+          <SidebarItem href="/logIn" aria-label="Log In">
+            Log In
+          </SidebarItem>
+          <SidebarItem href="/signUp" aria-label="Sign Up">
+            Sign Up
+          </SidebarItem>
+        </SidebarSection>
+      )}
+    </Sidebar>
+  );
+
+  const navbar = (
+    <Navbar>
+      <NavbarItem href="/" aria-label="Home" current>
+        Home
+      </NavbarItem>
+
+      <NavbarSpacer />
+
+      {authenticated ? (
+        <NavbarSection>
+          <NavbarItem href="/logOut" aria-label="Log Out">
+            Log Out
+          </NavbarItem>
+        </NavbarSection>
+      ) : (
+        <NavbarSection>
+          <NavbarItem href="/logIn" aria-label="Log In">
+            Log In
+          </NavbarItem>
+          <NavbarItem href="/signUp" aria-label="Sign Up">
+            Sign Up
+          </NavbarItem>
+        </NavbarSection>
+      )}
+    </Navbar>
+  );
 
   return (
-    <>
-      <div>Root</div>
-      <div>{authenticated ? "Logged In" : "Logged Out"}</div>
-      <button aria-label="Signup" onClick={() => console.log("1")}>
-        Signup
-      </button>
-      <button aria-label="Login" onClick={() => dispatch(login())}>
-        Login
-      </button>
-      <button aria-label="Login" onClick={() => dispatch(logout())}>
-        Logout
-      </button>
+    <StackedLayout sidebar={sidebar} navbar={navbar}>
       <Outlet />
-    </>
+    </StackedLayout>
   );
 };
 
