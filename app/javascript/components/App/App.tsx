839f3ae5d8bcdec4d3540ac5092bc8736b97f27c
John Huynh <john@huynh.io>
2024-08-27 23:19:29 -0500

    [boilerplate][feat] fix rails to react props lint warning and add comment for when we need it

--- app/javascript/components/App/App.tsx
+++ app/javascript/components/App/App.tsx
@@ -4,6 +4,9 @@ import Router from "components/App/Router";
 import { RouterProvider } from "react-router-dom";
 import { store } from "store";
 
+// Kept here for reference if we ever need props from Rails on bootup.
+//
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const App = (props) => {
   return (
     <Provider store={store}>
