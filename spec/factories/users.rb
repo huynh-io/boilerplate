36e05f0b2843f1dbaad25f57b64d26471a0e0011
John Huynh <john@huynh.io>
2024-08-28 14:27:12 -0500

    [boilerplate][feat] add annotate gem for annotating models/specs/factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :uuid             not null, primary key
+#  custom_metadata :jsonb
+#  email           :string
+#  email_verified  :boolean          default(FALSE), not null
+#  refresh_token   :string
+#  username        :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email     (email) UNIQUE
+#  index_users_on_username  (username) UNIQUE
+#
 FactoryBot.define do
   factory :user do # rubocop:disable Lint/EmptyBlock
   end
