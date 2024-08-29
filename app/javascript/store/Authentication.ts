c3a15f476b70f66d756a64cb54a160713ca23f4b
John Huynh <john@huynh.io>
2024-08-28 23:33:34 -0500

    [boilerplate][feat] add sign out to authentication store

--- app/javascript/store/Authentication.ts
+++ app/javascript/store/Authentication.ts
@@ -1,16 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { firebaseAuth } from "services/Firebase";
+import { signOut as firebaseSignOut } from "firebase/auth";
 
 export interface AuthenticationState {
   authenticated: boolean;
   initialized: boolean;
+  signOutStatus: "idle" | "pending" | "succeeded" | "rejected";
   user: any;
 }
 
 const initialState: AuthenticationState = {
   authenticated: false,
   initialized: false,
+  signOutStatus: "idle",
   user: null,
 };
+
+export const signOut = createAsyncThunk(
+  "authentication/signOut",
+  async (args, thunkAPI) => {
+    firebaseSignOut(firebaseAuth);
+  }
+);
 
 export const authenticationSlice = createSlice({
   name: "authentication",
@@ -20,6 +31,21 @@ export const authenticationSlice = createSlice({
       state.authenticated = action.payload;
       state.initialized = true;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(signOut.pending, (state, action) => {
+        state.signOutStatus = "pending";
+        state.authenticated = true;
+      })
+      .addCase(signOut.fulfilled, (state, action) => {
+        state.signOutStatus = "succeeded";
+        state.authenticated = false;
+      })
+      .addCase(signOut.rejected, (state, action) => {
+        state.signOutStatus = "rejected";
+        state.authenticated = true;
+      });
   },
 });
 
