27069ff1d3563dd6486159eb28c2ab41fccec7b5
John Huynh <john@huynh.io>
2024-09-05 20:51:06 -0500

    [boilerplate][feat] add initial search ui

--- app/javascript/components/Home/Home.tsx
+++ app/javascript/components/Home/Home.tsx
@@ -1,12 +1,24 @@
-import { Heading } from "components/shared/Catalyst/heading";
+import { Input, InputGroup } from "components/shared/Catalyst/input";
+import { MagnifyingGlassIcon } from "@heroicons/react/16/solid";
 import React from "react";
+import { Button } from "components/shared/Catalyst/button";
 
 const Home = () => {
   return (
     <div className="flex w-full justify-center">
-      <div className="sm:w-72 md:w-1/2">
-        <Heading>Home</Heading>
+      <div className="w-96">
+        <InputGroup>
+          <MagnifyingGlassIcon />
+          <Input
+            name="search"
+            placeholder="I am craving&hellip;"
+            aria-label="Search"
+          />
+        </InputGroup>
       </div>
+      <Button className="ml-4" outline>
+        Search
+      </Button>
     </div>
   );
 };
