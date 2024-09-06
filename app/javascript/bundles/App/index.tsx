1dc0ca38507dd90966ce2a0813b3b3c5c81412ca
John Huynh <john@huynh.io>
2024-09-05 20:00:56 -0500

    [boilerplate][feat] fix minor ui scroll bug from random semi colon

--- app/javascript/bundles/App/index.tsx
+++ app/javascript/bundles/App/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import App from "components/App";
 import "./output.css";
 
-const AppBundle = (props) => <App {...props} />;
+const AppBundle = (props) => {
+  return <App {...props} />;
+};
 
 export default AppBundle;
