23a1e71ef23452d0aa47a8483a30f3b92011e34f
John Huynh <john@huynh.io>
2024-09-07 01:14:24 -0500

    [boilerplate][feat] remove useless footer

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/header";
-import Footer from "@/components/footer";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -36,7 +35,6 @@ export default function RootLayout({
           <div className="relative flex min-h-screen flex-col">
             <Header />
             <main className="flex-1">{children}</main>
-            <Footer />
           </div>
           <Toaster />
         </ThemeProvider>
