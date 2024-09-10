3eede79e9063214ce4fc7db2e6e5ea94e0e4bdeb
John Huynh <john@huynh.io>
2024-09-09 22:54:14 -0500

    [boilerplate][feat] add initial auth state, firebase utils, and authenticator component to check/update auth state from firebase

---
+++ frontend/src/components/authenticator.tsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { firebaseAuth } from "@/lib/firebase";
+import useStore, { AppState } from "@/lib/store";
+
+const Authenticator = (props: { children: any }) => {
+  const { authenticated, authenticate } = useStore((state: AppState) => {
+    return {
+      authenticated: state.authenticated,
+      authenticate: state.authenticate,
+    };
+  });
+
+  useEffect(() => {
+    firebaseAuth.onAuthStateChanged((user) => {
+      if (user) {
+        authenticate(true);
+      } else {
+        authenticate(false);
+      }
+    });
+  }, [authenticated]);
+
+  return props.children;
+};
+
+export default Authenticator;
--- frontend/src/lib/store.ts
+++ frontend/src/lib/store.ts
@@ -2,28 +2,36 @@ import { create } from "zustand";
 import { devtools, persist } from "zustand/middleware";
 import type {} from "@redux-devtools/extension";
 
-interface AppState {
+export interface AppState {
+  authenticated: boolean;
+  authenticate: (auth: boolean) => void;
   signUpWithEmail: () => void;
   signUpWithGoogle: () => void;
 }
 
-// TODO: slice up the store once this file gets too large
+// TODO:
+// - slice up the store once this file gets too large
+// - fix redux chrome devtools integration
 const useStore = create<AppState>()(
   devtools(
     persist(
-      (set, get) => {
-        return {
-          signUpWithEmail: () => {
-            console.log("Signing up with email");
-          },
-          signUpWithGoogle: () => {
-            console.log("Signing up with Google");
-          },
-        };
-      },
+      (set, get) => ({
+        authenticated: false,
+        authenticate: (auth: boolean) => {
+          set({ authenticated: auth });
+        },
+        signUpWithEmail: () => {
+          console.log("Signing up with email");
+        },
+        signUpWithGoogle: () => {
+          console.log("Signing up with Google");
+        },
+      }),
       {
         name: "bellaire-storage",
       }
     )
   )
 );
+
+export default useStore;
