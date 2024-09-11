05ff7506b77909555d6575c9a5f44a0cdd38085e
John Huynh <john@huynh.io>
2024-09-10 23:01:23 -0500

    [boilerplate][chore] update nextjs config so we can build static assets for production deployment

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  distDir: "build",
   env: {
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
     FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
