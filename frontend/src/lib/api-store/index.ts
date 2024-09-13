a06f786e7af86987b8bdf016893d806b2b9996cf
John Huynh <john@huynh.io>
2024-09-12 22:11:08 -0500

    [boilerplate][feat] update authenticator with initial logic to verify id token after getting firebase auth user

--- frontend/src/lib/api-store/index.ts
+++ frontend/src/lib/api-store/index.ts
@@ -1,5 +1,5 @@
 import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
-import { QueryClient, useQuery } from "@tanstack/react-query";
+import { QueryClient, useMutation, useQuery } from "@tanstack/react-query";
 
 export const apiQueryClient = new QueryClient({
   defaultOptions: {
@@ -32,6 +32,23 @@ export function useSuppliers() {
     queryKey: ["suppliers"],
     queryFn: async (): Promise<Array<Supplier>> => {
       const response = await fetch(`${apiUrl}/api/v1/suppliers`);
+      return await response.json();
+    },
+  });
+}
+
+export function useUsersVerifyIdToken() {
+  return useMutation({
+    mutationFn: async (id_token: string): Promise<Object> => {
+      const response = await fetch(`${apiUrl}/api/v1/users/verify_id_token`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id_token }),
+      });
+
       return await response.json();
     },
   });
