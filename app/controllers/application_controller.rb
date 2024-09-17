eca660af3884c65db7f87da5d7f742c9380df511
John Huynh <john@huynh.io>
2024-09-16 23:29:44 -0500

    [boilerplate][feat] set page headers for smarter front end pagination

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@
 
 class ApplicationController < ActionController::API
   include Pagy::Backend
+
+  after_action :inject_pagy_headers
+
+  private
+
+  def inject_pagy_headers
+    pagy_headers_merge(@pagy) if @pagy
+  end
 end
