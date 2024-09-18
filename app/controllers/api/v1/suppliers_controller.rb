99e03a52d21a7d309013abf5b2f309568208dd8e
John Huynh <john@huynh.io>
2024-09-17 22:56:32 -0500

    [boilerplate][feat] support basic search in api/v1/suppliers#index

--- app/controllers/api/v1/suppliers_controller.rb
+++ app/controllers/api/v1/suppliers_controller.rb
@@ -4,8 +4,16 @@ module Api
   module V1
     class SuppliersController < ApplicationController
       def index
-        @pagy, @suppliers = pagy(Supplier.all)
+        scope = Suppliers::Searcher.call(index_params[:search])
+        @pagy, @suppliers = pagy(scope)
+
         render :index, status: :ok
+      end
+
+      private
+
+      def index_params
+        params.permit(:page, :search)
       end
     end
   end
