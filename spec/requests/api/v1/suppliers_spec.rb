1fbcae6892185b43e6383b6f91185d788746a388
John Huynh <john@huynh.io>
2024-09-18 00:16:31 -0500

    [boilerplate][feat] add request spec for api/v1/suppliers?query=foo

--- spec/requests/api/v1/suppliers_spec.rb
+++ spec/requests/api/v1/suppliers_spec.rb
@@ -5,7 +5,7 @@ require 'requests_helper'
 
 RSpec.describe 'Api::V1::Suppliers' do
   describe 'GET /api/v1/suppliers' do
-    context 'without query params' do
+    context 'without any params' do
       let(:get_request) { get '/api/v1/suppliers' }
 
       before do
@@ -20,6 +20,34 @@ RSpec.describe 'Api::V1::Suppliers' do
       it 'returns a list of suppliers' do
         expect(response_body).to be_an_instance_of(Hash)
         expect(response_body['suppliers']).to be_an_instance_of(Array)
+
+        first_object = response_body['suppliers'].first
+        expect(first_object).to have_key('id')
+        expect(first_object).to have_key('name')
+        expect(first_object).to have_key('updated_at')
+        expect(first_object).to have_key('created_at')
+      end
+    end
+
+    context 'with query param' do
+      let(:get_request) { get '/api/v1/suppliers?query=3' }
+
+      before do
+        create(:supplier, name: 'Supplier 1')
+        create(:supplier, name: 'Supplier 2')
+        create(:supplier, name: 'Supplier 3')
+
+        get_request
+      end
+
+      it 'returns 200' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'returns a filtered list of suppliers' do
+        expect(response_body).to be_an_instance_of(Hash)
+        expect(response_body['suppliers']).to be_an_instance_of(Array)
+        expect(response_body['suppliers'].count).to eq(1)
 
         first_object = response_body['suppliers'].first
         expect(first_object).to have_key('id')
