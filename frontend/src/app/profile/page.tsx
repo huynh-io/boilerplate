23dbc289c1398095e8496acb649d63a069c51dc4
John Huynh <john@huynh.io>
2024-10-02 21:25:31 -0500

    [boilerplate][feat] add initial empty profile page

---
+++ frontend/src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Profile() {
+  return (
+    <div className="flex items-start justify-center min-h-screen bg-background p-10">
+      hi
+    </div>
+  );
+}
