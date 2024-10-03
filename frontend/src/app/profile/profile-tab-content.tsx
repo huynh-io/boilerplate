3ddf62cc093302982f6d982bc0250fe6b5a1f5b4
John Huynh <john@huynh.io>
2024-10-02 22:45:11 -0500

    [boilerplate][feat] fetch and render current user, proof of concept for authorized api endpoints

--- frontend/src/app/profile/profile-tab-content.tsx
+++ frontend/src/app/profile/profile-tab-content.tsx
@@ -12,13 +12,15 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Skeleton } from "@/components/ui/skeleton";
 import { TabsContent } from "@/components/ui/tabs";
-import { useSignOut } from "@/lib/api-store";
+import { useGetUsersMe, useSignOut } from "@/lib/api-store";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function ProfileTabContent() {
   const router = useRouter();
+  const { data: currentUser, isLoading } = useGetUsersMe();
 
   const { mutate: signOut, isSuccess: isSignOutSuccess } = useSignOut();
   useEffect(() => {
@@ -37,30 +39,34 @@ export default function ProfileTabContent() {
           </CardDescription>
         </CardHeader>
 
-        <CardContent className="space-y-4">
-          <div className="flex items-center space-x-4">
-            <Avatar className="w-20 h-20">
-              <AvatarImage
-                src="https://github.com/shadcn.png"
-                alt="User avatar"
-              />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            <div>
-              <h2 className="text-2xl font-bold">John Doe</h2>
-            </div>
-          </div>
+        {isLoading && <Skeleton className="w-[100px] h-[20px] rounded-full" />}
 
-          <div className="space-y-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              defaultValue="john.doe@example.com"
-              disabled={true}
-            />
-          </div>
-        </CardContent>
+        {!isLoading && currentUser && (
+          <CardContent className="space-y-4">
+            <div className="flex items-center space-x-4">
+              <Avatar className="w-20 h-20">
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="User avatar"
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <div>
+                <h2 className="text-2xl font-bold">{currentUser.email}</h2>
+              </div>
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                defaultValue={currentUser.email}
+                disabled={true}
+              />
+            </div>
+          </CardContent>
+        )}
 
         <CardFooter className="flex flex-col sm:flex-row justify-center items-center space-y-2 sm:space-y-0 sm:space-x-2">
           <Button
