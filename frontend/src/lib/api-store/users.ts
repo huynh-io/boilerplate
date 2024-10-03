695907618fa8d6b947510c339a47c43d426c66da
John Huynh <john@huynh.io>
2024-10-02 22:27:53 -0500

    [boilerplate][feat] add hook to hit api/v1/users/me

--- frontend/src/lib/api-store/users.ts
+++ frontend/src/lib/api-store/users.ts
@@ -1,8 +1,8 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { apiClient } from "./api-client";
 
 export type User = {
-  accessToken: string;
+  accessToken?: string;
   email: string;
   id: string;
 };
@@ -13,6 +13,17 @@ export function useUsersCreate() {
       const response = await apiClient.post("/api/v1/users", {
         id_token,
       });
+
+      return response.data;
+    },
+  });
+}
+
+export function useGetUsersMe() {
+  return useQuery({
+    queryKey: ["user"],
+    queryFn: async () => {
+      const response = await apiClient.get("/api/v1/users/me");
 
       return response.data;
     },
