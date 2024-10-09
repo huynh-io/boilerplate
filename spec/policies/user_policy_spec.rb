bdedb34f3781cd61e7260c7b134d3801d6114f9d
John Huynh <john@huynh.io>
2024-10-08 23:22:34 -0500

    [boilerplate][feat] update user policy spec to use pundit provided rspec dsl

--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -3,19 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe UserPolicy, type: :policy do
-  describe 'me?' do
+  permissions :me? do
     let(:current_user) { create(:user) }
     let(:another_user) { create(:user) }
 
     context 'when the user is the record' do
       it 'grants access' do
-        expect(described_class.new(current_user, current_user).me?).to be(true)
+        expect(described_class).to permit(current_user, current_user)
       end
     end
 
     context 'when the user is not the record' do
       it 'does not grant access' do
-        expect(described_class.new(current_user, another_user).me?).to be(false)
+        expect(described_class).not_to permit(current_user, another_user)
       end
     end
   end
