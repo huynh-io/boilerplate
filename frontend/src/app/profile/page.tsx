ca988fb3daa8f87f98e6db1b16f55b76535d9f90
John Huynh <john@huynh.io>
2024-10-09 20:40:31 -0500

    [boilerplate][feat] standardize page padding with p-4

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -7,7 +7,7 @@ import OrderTabContent from "./order-tab-content";
 
 export default function Profile() {
   return (
-    <div className="flex items-start justify-center min-h-screen bg-background p-10">
+    <div className="flex items-start justify-center min-h-screen bg-background p-4">
       <Tabs defaultValue="profile" className="w-full max-w-3xl">
         <ProfileTabList />
 
