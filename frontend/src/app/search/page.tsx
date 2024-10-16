d0caeb42920d3a9dcf1249afd01038615a5e4b49
John Huynh <john@huynh.io>
2024-10-15 22:53:37 -0500

    [boilerplate][feat] linelength auto format

--- frontend/src/app/search/page.tsx
+++ frontend/src/app/search/page.tsx
@@ -12,10 +12,9 @@ export default function Search() {
   const searchParams = useSearchParams();
   const query = searchParams.get("q") ?? undefined;
 
-  const { error, isError, isFetching, data, fetchNextPage, hasNextPage } =
-    useSearch({
-      query: query,
-    });
+  const { error, isError, isFetching, data, fetchNextPage, hasNextPage } = useSearch({
+    query: query,
+  });
 
   const onLoadMore = debounce(() => {
     if (!isFetching) {
@@ -34,13 +33,8 @@ export default function Search() {
   const catalogItems = (data?.pages.flat() as CatalogItem[]) ?? [];
 
   const catalogItemsList = catalogItems.map((catalogItem: CatalogItem) => (
-    <li
-      key={catalogItem.id}
-      className="border rounded-lg p-4 hover:bg-gray-50 dark:hover:bg-gray-900"
-    >
-      <h2 className="text-xl font-semibold mb-2">
-        {catalogItem.itemData.name}
-      </h2>
+    <li key={catalogItem.id} className="border rounded-lg p-4 hover:bg-gray-50 dark:hover:bg-gray-900">
+      <h2 className="text-xl font-semibold mb-2">{catalogItem.itemData.name}</h2>
       <div className="text-sm text-gray-600 space-y-1">
         <p className="flex items-center">
           <MapPinIcon className="mr-2 h-4 w-4" />
